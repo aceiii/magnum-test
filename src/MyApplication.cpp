68246cf92f570c784cbb7a96c0034dc4d807dff5
Borin Ouch <borin.ouch@gmail.com>
2018-03-07 17:59:39 -0800

    Draw triangle

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -4,8 +4,23 @@
 #include <Magnum/Renderer.h>
 #include <Magnum/Version.h>
 #include <Magnum/Math/Color.h>
+#include <Magnum/Buffer.h>
+#include <Magnum/Mesh.h>
+#include <Magnum/Shaders/VertexColor.h>
 
 using namespace Magnum;
+using namespace Magnum::Math::Literals;
+
+struct TriangleVertex {
+    Vector2 position;
+    Color3 color;
+};
+
+const TriangleVertex data[] {
+    {{-0.5f, -0.5f}, 0xff0000_rgbf},
+    {{ 0.5f, -0.5f}, 0x00ff00_rgbf},
+    {{ 0.0f,  0.5f}, 0x0000ff_rgbf},
+};
 
 class MyApplication: public Platform::Application {
     public:
@@ -13,19 +28,35 @@ class MyApplication: public Platform::Application {
 
     private:
         void drawEvent() override;
+
+        Buffer _buffer;
+        Mesh _mesh;
+        Shaders::VertexColor2D _shader;
 };
 
-MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments} {
+MyApplication::MyApplication(const Arguments& arguments)
+    : Platform::Application{arguments, Configuration {}.setTitle("Triangles!!1")}
+{
     using namespace Magnum::Math::Literals;
 
     Renderer::setClearColor(0xa5c9ea_rgbf);
 
     Debug {} << "Hello! This application is running on" << Context::current().version()
              << "using" << Context::current().rendererString();
+
+    _buffer.setData(data, BufferUsage::StaticDraw);
+
+    _mesh.setPrimitive(MeshPrimitive::Triangles)
+        .setCount(3)
+        .addVertexBuffer(_buffer, 0,
+            Shaders::VertexColor2D::Position {},
+            Shaders::VertexColor2D::Color {Shaders::VertexColor2D::Color::Components::Three});
 }
 
 void MyApplication::drawEvent() {
     defaultFramebuffer.clear(FramebufferClear::Color);
+
+    _mesh.draw(_shader);
 
     swapBuffers();
 }
